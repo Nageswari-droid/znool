6ba9c9ca95397552e63cb929686f9ca7cf5a28c6
Nageswari-droid <nachasenthil@gmail.com>
2025-05-22 20:22:21 -0700

    Add sample data

---
+++ Server/src/books/model.js
@@ -0,0 +1,25 @@
+const books = {
+  1: {
+    id: 1,
+    title: "The Lord of the Rings",
+    author: "J.R.R. Tolkien",
+    genre: "Fantasy",
+    year: 1954,
+  },
+  2: {
+    id: 2,
+    title: "The Hobbit",
+    author: "J.R.R. Tolkien",
+    genre: "Fantasy",
+    year: 1937,
+  },
+  3: {
+    id: 3,
+    title: "The Silmarillion",
+    author: "J.R.R. Tolkien",
+    genre: "Fantasy",
+    year: 1977,
+  },
+};
+
+module.exports = { books };
