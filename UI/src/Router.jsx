d96b5b361e12b9a688df844bdde788176c2ad075
Nageswari-droid <nachasenthil@gmail.com>
2025-05-23 20:15:48 -0700

    UI initial setup

---
+++ UI/src/Router.jsx
@@ -0,0 +1,9 @@
+import { Route, Routes, BrowserRouter } from "react-router";
+
+export default function Router() {
+  <BrowserRouter>
+    <Route>
+      <Route path="/"></Route>
+    </Route>
+  </BrowserRouter>;
+}
